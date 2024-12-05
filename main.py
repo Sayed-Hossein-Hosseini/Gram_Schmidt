22473127110d9079d0f7959c4fce10542d387628
Sayed Hossein Hosseini Dolat Abadi <s.hossein.hosseini1381@gmail.com>
2024-12-06 03:00:27 +0330

    Calculating the projection of vectors

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
 import numpy as np
 # import pandas as pd
 
+vectors = np.array([])
+orthogonal_vectors = np.array([])
+orthogonal_unit_vectors = np.array([])
+
 def get_vectors():
     number_vector = int(input("Please enter the number of vectors : "))
     dimension = int(input("Please enter the dimension or size of the array : "))
 
-    u = np.array([])
+    vectors = np.array([])
 
     # Taking values from input and putting them into an array
     for x in np.arange(number_vector) :
         print("Vector", x + 1, ": ")
 
         for y in np.arange(dimension) :
-            u = np.append(u, int(input())).astype(int)
+            vectors = np.append(vectors, int(input())).astype(int)
 
     # Converting an array to a column and placing the members of an array in a column
-    u = u.reshape(number_vector, dimension).T
+    vectors = vectors.reshape(number_vector, dimension).T
 
-    return u
+
+def projection(number_vector, number_orthgonal_vector):
+    inner_v_u = vectors[number_vector - 1].dot(orthogonal_vectors[number_orthgonal_vector - 1])
+    inner_u_u = orthogonal_vectors[number_orthgonal_vector - 1].dot(orthogonal_vectors[number_orthgonal_vector - 1])
+    proj = (inner_v_u / inner_u_u) * orthogonal_vectors[number_orthgonal_vector - 1]
+
+    return proj
